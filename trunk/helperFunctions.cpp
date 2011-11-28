d5244ced0ddf64b8527257cba299e6c642e08873
arrived1@gmail.com <arrived1@gmail.com@210417b0-25ba-ea67-fa1b-d881887aac40>
2011-11-28 00:00:28 +0000

    clean up after randering particles, create shaders file

--- trunk/helperFunctions.cpp
+++ trunk/helperFunctions.cpp
@@ -1,5 +1,37 @@
 #include <helperFunctions.hpp>
 
+GLuint compileProgram(const char *vsource, const char *fsource)
+{
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(vertexShader, 1, &vsource, 0);
+    glShaderSource(fragmentShader, 1, &fsource, 0);
+    
+    glCompileShader(vertexShader);
+    glCompileShader(fragmentShader);
+
+    GLuint program = glCreateProgram();
+
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+
+    glLinkProgram(program);
+
+    // check if program linked
+    GLint success = 0;
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+
+    if (!success) {
+        char temp[256];
+        glGetProgramInfoLog(program, 256, 0, temp);
+        printf("Failed to link program:\n%s\n", temp);
+        glDeleteProgram(program);
+        program = 0;
+    }
+
+    return program;
+}
 
 void renderBox(int boxSize)
 {
